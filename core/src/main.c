3db0a098aae82231ca841668eaa426cc09614fc4
lucosmo <lukasz.kosmaczewski@gmail.com>
2022-01-12 12:37:44 +0000

    writing list to binary file

--- core/src/main.c
+++ core/src/main.c
@@ -5,6 +5,14 @@
 
 
 int main() {
+	
+	char* filename = "books";
+	FILE* input_file = fopen(filename, "wb+");
+    if (!output_file) {
+		perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
     Book b1 = {1,"title1","name1"};
     Book b2 = {2,"title2","name2"};
     Book b3 = {3,"title3","name3"};
@@ -18,10 +26,20 @@ int main() {
     insertOnBeginning(list,&b4);
 */
     insertAtEnd(list,&b1);
+    fwrite(b1, 1, sizeof Book, input_file);
+    printf("Done Writing!\n");
     insertAtEnd(list,&b2);
+    fwrite(b2, 1, sizeof Book, input_file);
+    printf("Done Writing!\n");
     insertAtEnd(list,&b3);
+    fwrite(b3, 1, sizeof Book, input_file);
+    printf("Done Writing!\n");
     insertAtEnd(list,&b4);
+    fwrite(b4, 1, sizeof Book, input_file);
+    printf("Done Writing!\n");
+    fclose(input_file);
     display(list);
+    
     freeList(list);
 
     return 0;
