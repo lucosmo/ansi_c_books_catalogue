090d2b6936bba667d24d4b323bb1414b8810d8ab
lucosmo <lukasz.kosmaczewski@gmail.com>
2022-01-13 20:42:35 +0000

    fixed writing to binary file

--- core/src/main.c
+++ core/src/main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "book.h"
 #include "list.h"
 
 
 int main() {
-	
+
 	char* filename = "books";
 	FILE* input_file = fopen(filename, "wb+");
-    if (!output_file) {
+    if (!input_file) {
 		perror("fopen");
         exit(EXIT_FAILURE);
     }
@@ -26,20 +27,20 @@ int main() {
     insertOnBeginning(list,&b4);
 */
     insertAtEnd(list,&b1);
-    fwrite(b1, 1, sizeof Book, input_file);
+    fwrite(&b1, 1, sizeof(Book), input_file);
     printf("Done Writing!\n");
     insertAtEnd(list,&b2);
-    fwrite(b2, 1, sizeof Book, input_file);
+    fwrite(&b2, 1, sizeof(Book), input_file);
     printf("Done Writing!\n");
     insertAtEnd(list,&b3);
-    fwrite(b3, 1, sizeof Book, input_file);
+    fwrite(&b3, 1, sizeof(Book), input_file);
     printf("Done Writing!\n");
     insertAtEnd(list,&b4);
-    fwrite(b4, 1, sizeof Book, input_file);
+    fwrite(&b4, 1, sizeof(Book), input_file);
     printf("Done Writing!\n");
     fclose(input_file);
     display(list);
-    
+
     freeList(list);
 
     return 0;
